95177f602bea81467d69991a776a436962512cb6
Mariia Kryvokhata <m.krivokhata@gmail.com>
2023-12-03 01:20:00 +0200

    Add right settings of the repository

--- publish.sh
+++ publish.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:kol-oss/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/kol-oss/database-coursework master:gh-pages
+git push -f https://github.com/mari4kaa/database-coursework.git master:gh-pages
 
 cd -
